b1eb71cb0aab3e0ae10a81b626914ad74bfa254a
TheShaylaMarie <shaylamariedesigns@gmail.com>
2025-01-01 13:56:32 -0600

    photo files added, api font key, added two color pickers for fonts

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Home/home.css"
 
 export default function home() {
 
+const [quoteData, setQuoteData] = useState([]);
+const [fontData, setFontData] = useState([]);
+const [colorData, setColorData] = useState({color: "black", WebkitTextStrokeColor: "white" });
+
+const api_url = "https://www.googleapis.com/webfonts/v1/webfonts?key="
+const api_key = import.meta.env.VITE_API_KEY_FONTS;
+// console.log(api_key);
 
 
+function captureColorData(colorValue, colorPicker) {
+//    console.log(colorValue, colorPicker);
+
+    if(colorPicker === "text-color") {
+        setColorData({...colorData, color: colorValue})
+    }
+    else {
+        setColorData({...colorData, WebkitTextStrokeColor: colorValue})
+    }
+}
 
   return (
     <>
@@ -12,8 +29,8 @@ export default function home() {
         <h4>Quote Generator</h4>
     <div className='card-container'>
         
-        <blockquote >
-            <p className='quote-container'>"Quote"</p>
+        <blockquote className='quote-container'>
+            <p className='quote-text' style={colorData} >"Quote"</p>
         </blockquote> 
 
 
@@ -22,6 +39,17 @@ export default function home() {
         <button>Random</button>
         <button>New Image</button>
         <button>New Quote</button>
+        <input type="color" 
+                id="quote-text-color"
+                className="color-picker"
+                onChange={(e) => captureColorData(e.target.value, "text-color")} 
+        />
+        <input type="color" 
+                id="quote-stroke-color"
+                className="color-picker"
+                onChange={(e) => captureColorData(e.target.value, "stroke-color")} 
+        />
+        
         </div>
     </div>
     </>
