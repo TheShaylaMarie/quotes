89a91e5d088651903b3f33a5db52735e7e5590a4
TheShaylaMarie <shaylamariedesigns@gmail.com>
2025-01-07 16:49:20 -0600

    complete random font API and starting on getting random fonts to load upon certain actions.

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import "../Home/home.css"
 import axios from 'axios';
-import Images from './Images';
 import imagesList from './Images';
 
 
@@ -9,140 +8,181 @@ import imagesList from './Images';
 
 export default function home() {
 
-const [quoteData, setQuoteData] = useState([]);
-const [fontData, setFontData] = useState([]);
-const [colorData, setColorData] = useState({color: "black", WebkitTextStrokeColor: "white" });
+    const [quoteData, setQuoteData] = useState([]);
+    const [quote, setQuote] = useState(localStorage.getItem("quote"));
+
+    const [fontData, setFontData] = useState([]);
+    const [font, setFont] = useState([]);
+
+    const [colorData, setColorData] = useState({ color: "black", WebkitTextStrokeColor: "white" });
+
+    const [image, setImage] = useState(Math.floor(Math.random() * imagesList.length));
+
+    const [favorite, setFavorite] = useState([]);
+
+    
+    const [favoritesObj, setFavoritesObj] = useState(localStorage.getItem("favorite_obj"))
+
+    const api_url_font = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyA7NxR8gN0q3-BLzasjsyjCLcVNqJvrqis"
+    const api_key_font = import.meta.env.VITE_API_KEY_FONTS;
 
 
-const [favorite, setFavorite] = useState([]);
-const [quote, setQuote] = useState(localStorage.getItem("quote"));
-const [favoritesObj, setFavoritesObj] = useState(localStorage.getItem("favorite_obj"))
-
-const api_url_font = "https://www.googleapis.com/webfonts/v1/webfonts?key="
-const api_key_font = import.meta.env.VITE_API_KEY_FONTS;
-// console.log(api_key_font);
-
-const api_url_quotes = "https://api.api-ninjas.com/v1/quotes";
-const api_key_quotes = import.meta.env.VITE_API_KEY_QUOTES;
-// const api_function_quotes = ""
+    const api_url_quotes = "https://api.api-ninjas.com/v1/quotes";
+    const api_key_quotes = import.meta.env.VITE_API_KEY_QUOTES;
 
 
+    useEffect(() => {
+        fetchFonts();
+    }, []);
 
-// Capture user selected colors for font and stroke
-function captureColorData(colorValue, colorPicker) {
-//    console.log(colorValue, colorPicker);
+    useEffect(() => {
+        localStorage.setItem("favorites", favorite)
+    }, [favorite])
 
-const favorite_object = {}
+    useEffect(() => {
+        localStorage.setItem("quote", quote)
+    }, [quote])
 
-    if(colorPicker === "text-color") {
-        setColorData({...colorData, color: colorValue})
-    }
-    else {
-        setColorData({...colorData, WebkitTextStrokeColor: colorValue})
-    }
-}
-
-
-
-
-useEffect(() =>{
-    localStorage.setItem("favorites", favorite)
-}, [favorite])
-
-useEffect(() =>{
-    localStorage.setItem("quote", quote)
-}, [quote])
-
-useEffect(() =>{
-    localStorage.setItem("favorite_obj", favoritesObj)
-}, [favoritesObj])
+    useEffect(() => {
+        localStorage.setItem("favorite_obj", favoritesObj)
+    }, [favoritesObj])
 
 
-function saveFavoriteObj() {
-    setFavoritesObj(favoritesObj.push(
-        {
-        quote: quote,
-        color: "red",
-        author: "Someone"
-    }
-    ))
-}
+// CAPTURE USER SELECTED COLORS FOR FONT AND STROKE
+    function captureColorData(colorValue, colorPicker) {
+        //    console.log(colorValue, colorPicker);
 
+        const favorite_object = {}
 
-
-function getQuote() {
-
-    axios.get(api_url_quotes, {
-        headers: {
-            "X-Api-Key": api_key_quotes
+        if (colorPicker === "text-color") {
+            setColorData({ ...colorData, color: colorValue })
         }
-    })
-    .then(response => {
-        if(response.data.length > 0) {
-            setQuoteData(response.data);
-            setQuote(response.data[0].quote);
+        else {
+            setColorData({ ...colorData, WebkitTextStrokeColor: colorValue })
         }
-    })
-    .catch((err) => {
-        console.log("Error retrieving quote data.", err);
-        
-    })
+    }
 
+
+
+
+
+
+// SAVE FAVORITES
+    function saveFavoriteObj() {
+        setFavoritesObj(favoritesObj.push(
+            {
+                quote: quote,
+                color: "red",
+                author: "Someone"
+            }
+        ))
+    }
+
+// GET FONTS FROM API
+    function fetchFonts() {
+        axios.get(api_url_font)
+        .then(response => {
+            const fonts = response.data.items;
+            if (fonts && fonts.length > 0 ) {
+                const randomFont = fonts[Math.floor(Math.random() * fonts.length)];
+                setFontData(fonts);
+                setFont(randomFont.family);
+            }
+            console.log("response:", response)
+        })
+        .catch((err) => {
+            console.log("Error recieving font data", err)
+        })
+    }
+
+
+    function getRandomFont() {
+
+    }
+
+
+// GET RANDOM QUOTE
+    function getQuote() {
+
+        axios.get(api_url_quotes, {
+            headers: {
+                "X-Api-Key": api_key_quotes
+            }
+        })
+            .then(response => {
+                if (response.data.length > 0) {
+                    setQuoteData(response.data);
+                    setQuote(response.data[0].quote);
+                }
+            })
+            .catch((err) => {
+                console.log("Error retrieving quote data.", err);
+
+            })
+
+    }
+
+// GET RANDOM IMAGE
+function getImage() {
+    setImage(Math.floor(Math.random() * imagesList.length));
 }
 
+    return (
+        <>
+            <h1>Inspire</h1>
+            <h4>Quote Generator</h4>
+            <div className='card-container'>
 
+                <blockquote className='quote-container'>
+                    <div className='img-container'>
+                        <img
+                            key={image}
+                            src={imagesList[image].src}
+                            alt={imagesList[image].alt}
+                            style={{ width: "100%", maxWidth: "auto" }}
+                        />
 
-  return (
-    <>
-        <h1>Inspire</h1>
-        <h4>Quote Generator</h4>
-    <div className='card-container'>
-        
-        <blockquote className='quote-container'>
-            <div className='img-container'>
-                {imagesList.map((image, index) =>
-                <img
-                key={index}
-                src={image.src}
-                alt={image.alt}
-                style={{ width: "100%", maxWidth: "300px", margin: "10px" }}
-                />)}
-            </div>
-            <div className='quote-text' style={colorData} >
-                {quoteData.map((quoteObj, index) => (
-                    <div key={index}>
-                        <p>"{quoteObj.quote}"</p>
-                        <small> {quoteObj.author} </small>
                     </div>
-                ))}
+                    <div className='quote-text' style={{...colorData,
+                    fontFamily: font || 'inherit'
+
+                    }}>
+                        {quoteData.map((quoteObj, index) => (
+                            <div key={index}>
+                                <p>"{quoteObj.quote}"</p>
+                                <small> {quoteObj.author} </small>
+                            </div>
+                        ))}
+                    </div>
+                </blockquote>
+
+
+
+
+                <div className='button-container' >
+                    <button onClick={() => getQuote()}>Random</button>
+
+                    <button onClick={() => getImage()}>New Image</button>
+
+                    <button onClick={() => getQuote()}>New Quote</button>
+
+                    <button onClick={() => getFont()}>New Font</button>
+
+                    <button>Favorite</button>
+
+                    <input type="color"
+                        id="quote-text-color"
+                        className="color-picker"
+                        onChange={(e) => captureColorData(e.target.value, "text-color")}
+                    />
+                    <input type="color"
+                        id="quote-stroke-color"
+                        className="color-picker"
+                        onChange={(e) => captureColorData(e.target.value, "stroke-color")}
+                    />
+
+                </div>
             </div>
-        </blockquote> 
-
-       
-
-
-        <div className='button-container' >
-        <button onClick={() => getQuote()}>Random</button>
-
-        <button>New Image</button>
-
-        <button onClick={() => getQuote()}>New Quote</button>
-
-        <button>Favorite</button>
-
-        <input type="color" 
-                id="quote-text-color"
-                className="color-picker"
-                onChange={(e) => captureColorData(e.target.value, "text-color")} 
-        />
-        <input type="color" 
-                id="quote-stroke-color"
-                className="color-picker"
-                onChange={(e) => captureColorData(e.target.value, "stroke-color")} 
-        />
-        
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
