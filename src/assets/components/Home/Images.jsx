8f4dc5c10c2d4e1fa27f27e533e1bf56db975e02
TheShaylaMarie <shaylamariedesigns@gmail.com>
2025-01-09 20:40:45 -0600

    add alt text to all images

--- src/assets/components/Home/Images.jsx
+++ src/assets/components/Home/Images.jsx
@@ -31,127 +31,127 @@ const imagesList = [
     {
         id: 1,
         src: img01,
-        alt: ""
+        alt: "calming view of clear turquoise water in the foreground with fluffy white clouds in the background"
     },
     {
         id: 2,
         src: img02,
-        alt: ""
+        alt: "A scenic mountainous landscape captured from the back of a camper van. The foreground shows the edge of the van's window, while the background highlights rolling hills, dense forests, and a misty valley"
     },
     {
         id: 3,
         src: img03,
-        alt: ""
+        alt: "A vivid landscape of Bryce Canyon, showcasing its iconic red and orange rock formations. The intricate layers of cliffs and sparse green vegetation contrast against a backdrop of distant mountains under a clear sky."
     },
     {
         id: 4,
         src: img04,
-        alt: ""
+        alt: "Landscape of calm waters with bright green tall grass on the water's edge. The beautiful mountains in the background are complimented by a pleasing cloudy sky at sunset "
     },
     {
         id: 5,
         src: img05,
-        alt: ""
+        alt: "A misty mountaintop view with bright orange flowers in the foreground. The golden sunrise is a nice contrast with the dark blue sky."
     },
     {
         id: 6,
         src: img06,
-        alt: ""
+        alt: "A calm ocean view of only water. The ocean is a deep blue at the foreground and fades toward the sky. The horizon line and the lighter blue of the sky is an eye catching contrast."
     },
     {
         id: 7,
         src: img07,
-        alt: ""
+        alt: "A serene mountain landscape overlooking a bright green field of grass at sunrise. The gently sloping, misty hills behind a deep forest green treeline of evergreens pulls you into its calming view."
     },
     {
         id: 8,
         src: img08,
-        alt: ""
+        alt: "A bright sky view of a sunset, the tall evergreens at the treeline are cast into the bright orange glow. In the foreground is a lightly sloping hill which is also bathed in the fiery rays."
     },
     {
         id: 9,
         src: img09,
-        alt: ""
+        alt: "The foreground is a weathered and rough dock overlooking shallow turquoise waters. The orange trees and foliage in the background have several waterfalls cascading through them."
     },
     {
         id: 10,
         src: img10,
-        alt: ""
+        alt: "A cliffside view of the massive orange and red canyons at Bryce Canyon National Park. There is sparse greenery growing throughout the crevices."
     },
     {
         id: 11,
         src: img11,
-        alt: ""
+        alt: "Standing in a field in autumn while the leaves are changing, some beautiful maple trees are already red. The background is dominated by the pleasing peak of Japan's Mount Fuji."
     },
     {
         id: 12,
         src: img12,
-        alt: ""
+        alt: "Breathtaking view of mountains at sunset, showcasing a foggy valley and rich green forest, enveloped in soft, warm hues."
     },
     {
         id: 13,
         src: img13,
-        alt: ""
+        alt: "The Milky Way, glorious in red, yellow, orange, and purple, glimmers above a serene Japanese tea farm, creating a magical night sky filled with stars."
     },
     {
         id: 14,
         src: img14,
-        alt: ""
+        alt: "The Milky Way, showcasing a rainbow of colors from yellow to red, pink, purple, and blue, glimmers brightly over a tranquil lake, with dark trees creating a beautiful silhouette in the foreground"
     },
     {
         id: 15,
         src: img15,
-        alt: ""
+        alt: "Colorful sunset over a calm ocean, with gentle waves lapping onto a sandy beach."
     },
     {
         id: 16,
         src: img16,
-        alt: ""
+        alt: "A stunning view of Moraine Lake in Banff National Park, Alberta, Canada, surrounded by majestic mountains, dense green forest, and vibrant blue waters."
     },
     {
         id: 17,
         src: img17,
-        alt: ""
+        alt: "A sunlit tree stands proudly in lush tall green grass, covered in dewy spider webs, its leaves shimmering in the warm morning sunlight filtering through."
     },
     {
         id: 18,
         src: img18,
-        alt: ""
+        alt: "A tranquil and inviting forest trail, flanked by majestic trees and rich green leaves, beckons adventurers into nature's embrace."
     },
     {
         id: 19,
         src: img19,
-        alt: ""
+        alt: "Breathtaking view of Norway's mountains towering over tranquil waters, showcasing nature's beauty in perfect harmony."
     },
     {
         id: 20,
         src: img20,
-        alt: ""
+        alt: "Delicate pink flowers thrive in the mountains, their beauty enhanced by the soft, warm light of a sunset sky."
     },
     {
         id: 21,
         src: img21,
-        alt: ""
+        alt: "A vibrant lavender field stretches under a bright blue sky, dotted with lush green trees and fluffy white clouds in the background."
     },
     {
         id: 22,
         src: img22,
-        alt: ""
+        alt: "The iconic Great Wall of China stretches majestically across the landscape, surrounded by misty morning light and lush green forest."
     },
     {
         id: 23,
         src: img23,
-        alt: ""
+        alt: "A tranquil countryside scene at dawn, featuring mist enveloping trees and gentle hills under a soft, hazy sky."
     },
     {
         id: 24,
         src: img24,
-        alt: ""
+        alt: "A breathtaking sunset casts colorful hues over a tranquil lake, which reflects its vibrant purple and pinks, framed by majestic mountains in the distance."
     },
     {
         id: 25,
         src: img25,
-        alt: ""
+        alt: "A delicate cream colored moth rests on a leaf, it's wings contrasting with the dark shadows of the surrounding greenery."
     }
 ]
 
